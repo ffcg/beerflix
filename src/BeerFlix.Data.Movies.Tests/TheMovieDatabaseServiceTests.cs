3135663581870e8eb7d52b95b0e68b502073358c
Arthur Onoszko <arthur.onoszko@ffcg.se>
2014-10-02 15:54:07 +0200

    Tests and updated themoviedatabaseproxy #1

--- src/BeerFlix.Data.Movies.Tests/TheMovieDatabaseServiceTests.cs
+++ src/BeerFlix.Data.Movies.Tests/TheMovieDatabaseServiceTests.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace BeerFlix.Data.Movies.Tests
 {
     [TestFixture]
     public class TheMovieDatabaseServiceTests
     {
-        [Test]
-        public void TheMovieDatabaseService_should_generate_correct_url_when_searching_movie()
-        {
-            //ARRANGE
-            
-            //ACT
-
-            //ASSERT
-        }
     }
 }
